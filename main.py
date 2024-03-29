261daf04b6e2c7870acc3f8a7ad584c58264174e
antonovmike <antonovma@outlook.com>
2024-03-29 13:58:52 +0400

    Delete post. HTTP status. HTTP exception

--- main.py
+++ main.py
@@ -21,6 +21,11 @@ def find_post(id):
         if p['id'] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
 @app.get("/")
 async def root():
     return {"message": "Welcome"}
@@ -45,10 +50,20 @@ def get_latest_post():
     return {"latest_detail": post}
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
+def get_post(id: int):
     post = find_post(id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f'post with id: {id} was not found')
 
     return {"post_detail": post}
+
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} does not exist")
+    my_posts.pop(index)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
