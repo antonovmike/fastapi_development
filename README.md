87945d9e6e162b1bc45fa0a80980f4bdd4cb0d14
antonovmike <antonovma@outlook.com>
2024-03-29 13:36:13 +0400

    HTTPException

--- README.md
+++ README.md
@@ -6,10 +6,16 @@ https://youtu.be/0sOvCWFmrtA
 
 https://youtube.com/playlist?list=PL8VzFQ8k4U1IiGUWdBI7s9Y7dm-4tgCXJ
 
+https://github.com/Sanjeev-Thiyagarajan/fastapi-course
+
 Course from Sanjeev Thiyagarajan
 
 ```
 uvicorn main:app --reload
 ```
 
-https://youtu.be/0sOvCWFmrtA?feature=shared&t=6767
+[HTTP request methods](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods)
+
+[HTTP response status codes](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
+
+https://youtu.be/0sOvCWFmrtA?feature=shared&t=7231
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -34,18 +34,21 @@ def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict['id'] = randrange(0, 100000)
     my_posts.append(post_dict)
+
     return {"data": my_posts}
 
-# Order matters
 @app.get("/posts/latest")
 def get_latest_post():
     post = my_posts[len(my_posts) - 1]
     print("Latest post:", post)
+
     return {"latest_detail": post}
 
-# Order matters
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(id)
-    print(post)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'post with id: {id} was not found')
+
     return {"post_detail": post}
