6945d5bc23fc85d5904b6e440d82737735ebe846
antonovmike <antonovma@outlook.com>
2024-04-07 10:18:41 +0400

    SQLAlchemy db.query(models.Post)

--- app/main.py
+++ app/main.py
@@ -69,9 +69,10 @@ def test_posts(db: Session = Depends(get_db)):
 
 
 @app.get("/posts")
-async def get_posts():
-    cursor.execute("""SELECT * from posts""")
-    posts = cursor.fetchall()
+async def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    # cursor.execute("""SELECT * from posts""")
+    # posts = cursor.fetchall()
     return {"data": posts}
 
 
