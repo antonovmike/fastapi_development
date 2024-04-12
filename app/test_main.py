51fdd3fc2a04b984021fe5ba207e3a14bbbbba03
antonovmike <antonovma@outlook.com>
2024-04-12 13:17:22 +0400

    added test_create_posts

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+from fastapi import status
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -7,5 +8,18 @@ client = TestClient(app)
 
 def test_root():
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "Welcome"}
+
+
+def test_create_posts():
+    test_data = {
+        "title": "demo",
+        "content": "demo"
+    }
+    
+    response = client.post("/posts", json=test_data)
+    
+    assert response.status_code == status.HTTP_201_CREATED
+    assert "title" in response.json()
+    assert "content" in response.json()
