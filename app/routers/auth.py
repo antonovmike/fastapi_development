1f73eb39d101963220b048fe781e32a217b5ac49
antonovmike <antonovma@outlook.com>
2024-04-19 08:13:06 +0400

    Login Process: pwd_contest renamed to pwd_context

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 
-from .. import database, schemas
+from .. import database, schemas, models
 
 router = APIRouter(tags=['Authentification'])
 
 
 @router.post('/login')
 def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    pass
+    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentials")
