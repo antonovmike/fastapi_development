15e0809a25d16cd49a48bb62bb1cb4af2f37210f
antonovmike <antonovma@outlook.com>
2024-04-19 08:03:02 +0400

    Login Process

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from sqlalchemy.orm import Session
+
+from .. import database
 
 router = APIRouter(tags=['Authentification'])
+
+
+@router.post('/login')
+def login(db: Session = Depends(database.get_db)):
+    pass
